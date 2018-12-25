b708216afbb186a4b825ce59dff73c9888f278c6
Pence <matt@ohiosveryown.co>
2018-12-25 15:43:53 -0500

    Removed Gsap

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,8 @@
 <!-- layout -->
 <template>
-  <section class="app">
+  <section>
 
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="special card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <!-- <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div> -->
+
 
   </section>
 </template>
@@ -20,25 +12,7 @@
 <style lang="scss">
   @import '@/assets/mq.scss';
 
-  .app {
-    border: 4px solid pink;
-    height: 100vh;
-    overflow-x: scroll;
-    display: flex;
-  }
 
-  .card {
-    --unit: 32px;
-    margin: var(--unit);
-    min-width: 37vw; height: 28.8rem;
-    // height: calc(100% - calc(var(--unit) * 2));
-    background: #fff;
-  }
-
-  .blue { background: blue; }
-  .red  { background: red; }
-
-  .none { transform: matrix(0, 0, 0, 0, 0, 0); }
 
 </style>
 
@@ -48,35 +22,7 @@
 
   export default {
     mounted() {
-      const special = document.querySelector('.special')
-      const cards = document.querySelector('.cards')
 
-      const main = document.querySelector('main')
-      const app = document.querySelector('.app')
-      let pageXOffset = window.pageXOffset
-
-      app.addEventListener('scroll', () => {
-        let x = app.scrollLeft
-
-        const newPageOffset = x
-        const diff = x - pageXOffset
-
-      	TweenMax.to(app, .75, {
-      		skewX: diff * -0.25,
-      		ease: Power4.ease
-      	})
-
-      	pageXOffset = newPageOffset
-        console.log(pageXOffset)
-
-        if (pageXOffset <= 20) {
-          special.classList.add('blue')
-          app.style.transform = "none !important"
-        }
-        else {
-          special.classList.remove('blue')
-        }
-      })
     }
   }
 
