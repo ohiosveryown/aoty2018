f521bf870845d82006252e568f45c82f46fca537
Pence <matt@ohiosveryown.co>
2018-12-30 13:39:34 -0500

    Matt List

--- components/Matt.vue
+++ components/Matt.vue
@@ -3,24 +3,125 @@
   <ul class="">
 
     <!-- 🎧 -->
-      <li class="">
-        <nuxt-link to='/test'>
-          <h3 class="f-mabry tar">5</h3>
-          <figure class="">
-            <img class="" src="@/assets/img/matt/10.jpg" alt="Saba">
-            <figcaption class="f-mabry">Care for Me — Rap/Hip-Hop</figcaption>
-            <h5 class="f-space">Saba</h5>
-          </figure>
-        </nuxt-link>
-      </li>
+    <li class="">
+      <nuxt-link to='/test'>
+        <h3 class="f-mabry tar">10</h3>
+        <figure class="">
+          <img class="" src="@/assets/img/matt/10.jpg" alt="Saba">
+          <figcaption class="f-mabry">Care for Me — Rap/Hip-Hop</figcaption>
+          <h5 class="f-space">Saba</h5>
+        </figure>
+      </nuxt-link>
+    </li>
 
+    <!-- 🎧 -->
+    <li class="">
+      <nuxt-link to='/test'>
+        <h3 class="f-mabry tar">9</h3>
+        <figure class="">
+          <img class="" src="@/assets/img/matt/09.jpg" alt="J.Cole">
+          <figcaption class="f-mabry">KOD — Rap/Hip-Hop</figcaption>
+          <h5 class="f-space">J.Cole</h5>
+        </figure>
+      </nuxt-link>
+    </li>
 
+    <!-- 🎧 -->
+    <li class="">
+      <nuxt-link to='/test'>
+        <h3 class="f-mabry tar">8</h3>
+        <figure class="">
+          <img class="" src="@/assets/img/matt/08.jpg" alt="Coutney Barnett">
+          <figcaption class="f-mabry">Tell Me How You <span>Really Feel</span> — Alternative</figcaption>
+          <h5 class="f-space">Coutney Barnett</h5>
+        </figure>
+      </nuxt-link>
+    </li>
 
-    <li>02</li>
-    <li>03</li>
-    <li>04</li>
-    <li>05</li>
-    <li>06</li>
+    <!-- 🎧 -->
+    <li class="">
+      <nuxt-link to='/test'>
+        <h3 class="f-mabry tar">7</h3>
+        <figure class="">
+          <img class="" src="@/assets/img/matt/07.jpg" alt="Sales">
+          <figcaption class="f-mabry">Forever and Ever — Alternative</figcaption>
+          <h5 class="f-space">Sales</h5>
+        </figure>
+      </nuxt-link>
+    </li>
+
+    <!-- 🎧 -->
+    <li class="">
+      <nuxt-link to='/test'>
+        <h3 class="f-mabry tar">6</h3>
+        <figure class="">
+          <img class="" src="@/assets/img/matt/06.jpg" alt="St. Beauty">
+          <figcaption class="f-mabry">Running to the Sun — R&amp;B</figcaption>
+          <h5 class="f-space">St. Beauty</h5>
+        </figure>
+      </nuxt-link>
+    </li>
+
+    <!-- 🎧 -->
+    <li class="">
+      <nuxt-link to='/test'>
+        <h3 class="f-mabry tar">5</h3>
+        <figure class="">
+          <img class="" src="@/assets/img/matt/05.jpg" alt="Arctic Monkeys">
+          <figcaption class="f-mabry">Tranquility Base <span>Hotel &amp; Casino</span> — Alternative</figcaption>
+          <h5 class="f-space">Arctic Monkeys</h5>
+        </figure>
+      </nuxt-link>
+    </li>
+
+    <!-- 🎧 -->
+    <li class="">
+      <nuxt-link to='/test'>
+        <h3 class="f-mabry tar">4</h3>
+        <figure class="">
+          <img class="" src="@/assets/img/matt/04.jpg" alt="Khruangbin">
+          <figcaption class="f-mabry">Con Todo <span>El Mundo</span> — Alternative</figcaption>
+          <h5 class="f-space">Khruangbin</h5>
+        </figure>
+      </nuxt-link>
+    </li>
+
+    <!-- 🎧 -->
+    <li class="">
+      <nuxt-link to='/test'>
+        <h3 class="f-mabry tar">3</h3>
+        <figure class="">
+          <img class="" src="@/assets/img/matt/03.jpg" alt="Kali Uchis">
+          <figcaption class="f-mabry">Isolation — R&amp;B/Soul</figcaption>
+          <h5 class="f-space">Kali Uchis</h5>
+        </figure>
+      </nuxt-link>
+    </li>
+
+    <!-- 🎧 -->
+    <li class="">
+      <nuxt-link to='/test'>
+        <h3 class="f-mabry tar">2</h3>
+        <figure class="">
+          <img class="" src="@/assets/img/matt/02.jpg" alt="Snoh Aalegra">
+          <figcaption class="f-mabry">FEELS — R&amp;B/Soul</figcaption>
+          <h5 class="f-space">Snoh Aalegra</h5>
+        </figure>
+      </nuxt-link>
+    </li>
+
+    <!-- 🎧 -->
+    <li class="">
+      <nuxt-link to='/test'>
+        <h3 class="f-mabry tar">1</h3>
+        <figure class="">
+          <img class="" src="@/assets/img/matt/01.jpg" alt="Tierra Whack">
+          <figcaption class="f-mabry">Whack World — Rap/Hip-Hop</figcaption>
+          <h5 class="f-space">Tierra Whack</h5>
+        </figure>
+      </nuxt-link>
+    </li>
+
 
   </ul>
 </template>
@@ -49,15 +150,12 @@
     flex: 0 0 80%;
     margin-right: 1.6rem;
     height: 64vh;
-    background: lightblue;
-    transition: transform .25s ease;
+    transition: transform 250ms ease;
     will-change: transform;
 
     @include breakpoint(md)  { height: 40rem; }
     @include breakpoint(mdl) { margin-top: -5.6rem; height: 28.8rem; flex-basis: 40%; }
   }
-
-  li:nth-of-type(1) { background: none; }
 
   li h3 { margin-bottom: 1rem; }
 
@@ -73,6 +171,11 @@
     height: 100%;
     object-fit: cover;
     @include breakpoint(md) { margin-bottom: 1.6rem; }
+  }
+
+  li span {
+    display: none;
+    @include breakpoint(md) { display: inline; }
   }
 
 </style>
