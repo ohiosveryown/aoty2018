203439a13256cc09e20bfc097b7ff17d4c6626de
Pence <matt@ohiosveryown.co>
2018-12-30 11:53:32 -0500

    Matt List

--- components/Matt.vue
+++ components/Matt.vue
@@ -5,10 +5,12 @@
 
       <li class="">
         <nuxt-link to='/test'>
-          <!-- <h3 class="f-mabry mb-2 tar">5</h3> -->
-          <figure class="debug">
-            <img src="@/assets/img/d01.jpg" alt="">
+          <h3 class="f-mabry mb-2 tar">5</h3>
+          <figure class="">
+            <img src="@/assets/img/matt/10.jpg" alt="Saba">
+            <figcaption class="f-mabry">Care for Me — Rap/Hip-Hop</figcaption>
           </figure>
+
         </nuxt-link>
       </li>
 
@@ -34,20 +36,23 @@
     max-height: 100vh; height: 100vh;
     overflow-x: scroll;
     -webkit-overflow-scrolling: touch;
-    &::-webkit-scrollbar { display: none; } // Safari and Chrome}
+    &::-webkit-scrollbar { display: none; } // safari & chrome
   }
 
   li {
     margin-right: 1.6rem;
-    height: 300px;
+    height: 28.8rem;
     flex: 0 0 34%;
     background: lightblue;
     transition: transform .25s ease;
     will-change: transform;
   }
 
+  li:nth-of-type(1) { background: none; }
+
   figure {
     display: flex;
+    flex-direction: column;
     height: 100%;
   }
 
@@ -60,7 +65,25 @@
 
   export default {
     mounted() {
+      const list = document.querySelector('ul')
+      const listContent = document.querySelector('li')
+      let currentPixel = list.scrollLeft
 
+      function mousePosition(e) {
+        if (e.y > 200) { scroll.style.opacity = '.8' }
+        else { scroll.style.opacity = '0' }
+      }
+
+      const looper = () => {
+        const newPixel = list.scrollLeft
+        const diff = newPixel - currentPixel
+        const speed = -diff * .15
+
+        list.style.transform = "skewX(" + speed +"deg)"
+        currentPixel = newPixel
+        requestAnimationFrame(looper)
+      }
+      looper()
     }
   }
 
