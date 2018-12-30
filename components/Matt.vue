1f69bc09240d566a2d8a1b4573426a22438c2cf7
Pence <matt@ohiosveryown.co>
2018-12-30 12:14:29 -0500

    Matt List

--- components/Matt.vue
+++ components/Matt.vue
@@ -5,12 +5,12 @@
 
       <li class="">
         <nuxt-link to='/test'>
-          <h3 class="f-mabry mb-2 tar">5</h3>
+          <h3 class="f-mabry tar">5</h3>
           <figure class="">
-            <img src="@/assets/img/matt/10.jpg" alt="Saba">
+            <img class="" src="@/assets/img/matt/10.jpg" alt="Saba">
             <figcaption class="f-mabry">Care for Me — Rap/Hip-Hop</figcaption>
           </figure>
-
+          <h5 class="f-space">Saba</h5>
         </nuxt-link>
       </li>
 
@@ -32,31 +32,47 @@
 
   ul {
     display: flex;
-    align-items: center;
     max-height: 100vh; height: 100vh;
+    overflow-y: hidden;
     overflow-x: scroll;
     -webkit-overflow-scrolling: touch;
     &::-webkit-scrollbar { display: none; } // safari & chrome
+
+    @include breakpoint(md) {
+      align-items: center;
+    }
   }
 
   li {
+    flex: 0 0 80%;
     margin-right: 1.6rem;
-    height: 28.8rem;
-    flex: 0 0 34%;
+    height: 64vh;
     background: lightblue;
     transition: transform .25s ease;
     will-change: transform;
+
+    @include breakpoint(mdl) { height: 28.8rem; flex-basis: 34%; }
   }
 
   li:nth-of-type(1) { background: none; }
 
-  figure {
+  li h3 { margin-bottom: 1rem; }
+
+  li figure {
     display: flex;
     flex-direction: column;
     height: 100%;
+    @include breakpoint(md) { margin-bottom: 4rem; }
   }
 
-  img { object-fit: cover; }
+
+  li img {
+    margin-bottom: .8rem;
+    height: 100%;
+    object-fit: cover;
+    @include breakpoint(md) { margin-bottom: 1.6rem; }
+  }
+
 </style>
 
 
