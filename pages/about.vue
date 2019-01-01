3e997bbf29c4227d477e5f7010be66c6716c37fb
Pence <matt@ohiosveryown.co>
2019-01-01 02:05:01 -0500

    About Lg Layout

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,11 @@
 <!-- layout -->
 <template>
-  <main class="debug mw">
+  <main class=" mw">
 
-    <section class="debug copy">
+    <section class=" copy">
       <header>
-        <h5 class="f-space">About</h5>
-        <h1 class="f-mabry">2018 Albums of the Year is a communal project built around music &amp; friendship.</h1>
+        <h5 class="mb-1 f-space infrared">About</h5>
+        <h1 class="mb-4 f-mabry">2018 Albums of the Year is a communal project built around music &amp; friendship.</h1>
       </header>
       <article class="">
         <p>2018 was a great year for music. To catalog this period of sonic mastery, myself and a group of friends want to share our favorites with you. This site was designed and built by me, Matt – and you can view the source here.</p>
@@ -13,18 +13,18 @@
       </article>
     </section>
 
-    <section class="debug lists">
-      <h5 class="f-space">Album Lists</h5>
+    <section class=" lists">
+      <h5 class="mb-1 f-space infrared">Album Lists</h5>
       <ul>
-        <li>
+        <li class="mb-4">
           <nuxt-link to='/Matt'><h3 class="f-mabry">Matt’s Top 10 Albums of the Year</h3></nuxt-link>
           <a class="f-space op-5" href="https://twitter.com/cmykw_" target="_blank">@cmykw_</a>
         </li>
-        <li>
+        <li class="mb-4">
           <nuxt-link to='/Matt'><h3 class="f-mabry">Stepfan's Top 5 Albums of the Year</h3></nuxt-link>
           <a class="f-space op-5" href="https://twitter.com/stepfdelaghetto" target="_blank">@stepfdelaghetto</a>
         </li>
-        <li>
+        <li class="mb-4">
           <nuxt-link to='/Matt'><h3 class="f-mabry">Kristie's Top 10 Albums of the Year</h3></nuxt-link>
           <a class="f-space op-5" href="https://twitter.com/kristie_feltner" target="_blank">@kristie_feltner</a>
         </li>
@@ -39,8 +39,12 @@
 <style lang="scss">
   @import '@/assets/mq.scss';
 
+  .infrared { color: var(--infrared); }
+
   main {
     display: flex;
+    position: relative;
+
     @include breakpoint(mdl) {
       height: 100vh;
       flex-direction: row;
@@ -49,15 +53,25 @@
   }
 
   .copy {
-    @include breakpoint(mdl) { width: grid-width(7); }
+    @include breakpoint(mdl) {
+      position: absolute;
+      top: 10%;
+      width: grid-width(8);
+    }
   }
 
   article {
-    @include breakpoint(mdl) { width: grid-width(8); }
+    @include breakpoint(mdl) {
+      width: grid-width(8);
+    }
   }
 
   .lists {
-    @include breakpoint(mdl) { margin-left: grid-width(0); width: grid-width(5); }
+    @include breakpoint(mdl) {
+      position: absolute;
+      bottom: 16%; right: 0;
+      width: grid-width(4);
+    }
   }
 
   ul {
