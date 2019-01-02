c9c6d39bae47527cdade47d416d27f9b06a25fd4
Pence <matt@ohiosveryown.co>
2019-01-01 21:06:58 -0500

    About Transitions

--- pages/about.vue
+++ pages/about.vue
@@ -2,6 +2,7 @@
 <template>
   <main class="mw">
 
+    <div class="cover-out"/>
     <aboutNavigation/>
 
     <section class="copy">
@@ -23,12 +24,20 @@
           <a class="f-space op-5" href="https://twitter.com/cmykw_" target="_blank">@cmykw_</a>
         </li>
         <li class="mb-4">
+          <h3 class="f-mabry">Stefan's Top 5 Albums of the Year</h3>
+          <span class="f-space op-5">check back later</span>
+        </li>
+        <!-- <li class="mb-4">
           <nuxt-link to='/'><h3 class="mb-0 f-mabry">Stepfan's Top 5 Albums of the Year</h3></nuxt-link>
           <a class="f-space op-5" href="https://twitter.com/stepfdelaghetto" target="_blank">@stepfdelaghetto</a>
-        </li>
+        </li> -->
         <li class="mb-4">
           <nuxt-link to='/Kristie'><h3 class="mb-0 f-mabry">Kristie's Top 5 Albums of the Year</h3></nuxt-link>
           <a class="f-space op-5" href="https://twitter.com/kristie_feltner" target="_blank">@kristie_feltner</a>
+        </li>
+        <li class="mb-4">
+          <h3 class="f-mabry">More Lists Coming Soon</h3>
+          <span class="f-space op-5">check back later</span>
         </li>
       </ul>
     </section>
@@ -40,6 +49,20 @@
 <!-- style -->
 <style scoped lang="scss">
   @import '@/assets/mq.scss';
+
+  .cover-out {
+    position: fixed;
+    top: 0; left: 0;
+    z-index: var(--zmax);
+    width: 100vw; height: 100vh;
+    background: #0b0b0b;
+    transform: scaleY(0);
+    transform-origin: top;
+    transition: all 800ms cubic-bezier(0.770, 0, 0.175, 1);
+    will-change: transform;
+  }
+
+  .cover-out-active { z-index: 99999; transform: scaleY(1); }
 
   .infrared { color: var(--infrared); }
   a { text-decoration: underline; }
@@ -86,7 +109,7 @@
 
     @include breakpoint(mdl) {
       position: absolute;
-      bottom: 16%; right: 0;
+      bottom: 8%; right: -2rem;
       width: grid-width(4);
     }
   }
@@ -101,7 +124,7 @@
   p { text-align: justify; }
   p + p { text-indent: 2ch; }
 
-  .aboutFade-enter-active, .aboutFade-leave-active { transition: all 200ms ease; }
+  .aboutFade-enter-active, .aboutFade-leave-active { transition: all 1200ms ease; }
   .aboutFade-enter, .aboutFade-active { opacity: 0; }
 
 </style>
@@ -117,7 +140,15 @@
     },
     components: { aboutNavigation },
     mounted() {
-
+      const logotype = document.querySelector('.logotype')
+      const exitTrigger = document.querySelector('.nav-trigger')
+      const coverOut = document.querySelector('.cover-out')
+      exitTrigger.addEventListener('click', () => {
+        coverOut.classList.add('cover-out-active')
+      })
+      logotype.addEventListener('click', () => {
+        coverOut.classList.add('cover-out-active')
+      })
     }
   }
 
