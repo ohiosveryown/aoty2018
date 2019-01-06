53072c4c7e83aa6764d86fb68b77bf68ed0c4702
Pence <matt@ohiosveryown.co>
2019-01-05 21:49:09 -0500

    Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,9 @@ module.exports = {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
+    // script: [
+    //   { src: 'https://unpkg.com/smooth-scrollbar@latest/dist/smooth-scrollbar.js' }
+    // ]
   },
 
   /*
